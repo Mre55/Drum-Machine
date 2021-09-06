fa34f67f8cd6bb9c59a2c6b0c3a74e7af346e95c
mre55 <misganawmihreteab>
2021-09-06 22:10:56 +0300

    First commit

---
+++ src/components/DrumMachine.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+class DrumMachine extends React.Component {
+
+      render() {
+            return (
+                  <h1>This is from Drum machine Component</h1>
+            )
+      }
+}
+
+export default DrumMachine;
